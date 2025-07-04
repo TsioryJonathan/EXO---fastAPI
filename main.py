5e920ffcf58e67e9f33b092858afdf6fcf3a86c7
Ryan Andriamahery <ryan@hei.school>
2025-07-04 13:00:33 +0300

    feat: POST /welcome

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, requests
+from pydantic import BaseModel
 from starlette.responses import JSONResponse
 
 app = FastAPI()
@@ -6,3 +7,10 @@ app = FastAPI()
 @app.get("/hello")
 def read_hello():
     return JSONResponse(content="Hello world", status_code=200)
+
+class WelcomeRequest(BaseModel):
+    name: str
+
+@app.post("/welcome")
+def welcome_user(request: WelcomeRequest):
+    return {f"Bienvenue {request.name}"}
