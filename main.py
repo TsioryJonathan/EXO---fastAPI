a5be5244afd854ecc316b6b291df8b90c417ba92
Jonathan <rakotoarisoajonathan0@gmail.com>
2025-07-10 08:22:04 +0300

    feat: custom-hello endpoint (optional parameters exo)

--- main.py
+++ main.py
@@ -22,4 +22,26 @@ class WelcomeRequest(BaseModel):
 def welcome_user(request: WelcomeRequest):
     return {f"Bienvenue {request.name}"}
 
+@app.post("/hello-custom")
+def read_hello_custom(request: Request, name: Optional[str] = None, is_teacher: Optional[bool] = None ):
+    accept_headers = request.headers.get("Accept")
+    if accept_headers != "text/plain":
+        return JSONResponse({"message": "Unsupported Media Type"}, status_code=400)
+
+    if name is None and is_teacher is None:
+        return RedirectResponse(url="/hello", status_code=303)
+
+    if name is not None and is_teacher is None:
+        is_teacher = False
+
+    if name is None and is_teacher is not None:
+        name = "Non fournie"
+
+    if is_teacher:
+        return "Hello Teacher " + name + " !"
+    else:
+        return "Hello " + name + " !"
+
+
+
 
